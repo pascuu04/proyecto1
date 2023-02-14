c72404a3c749a6810982c28855af748912688abc
KerryCoppo <dam_davidpascual@riberadeltajo.es>
2023-02-14 16:00:50 +0100

    Un syso + en la clase principal

--- prueba1/src/paquete/Principal.java
+++ prueba1/src/paquete/Principal.java
@@ -3,5 +3,6 @@ package paquete;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("El cambio a la clase Principal");
 	}
 }
